b8df104acc7230c266494a50910ba1e39a1c64c3
anxoret <post.lux@yandex.ru>
2019-10-06 22:56:52 +0300

    Create a list with help of React

---
+++ learning-react-by-alex-banks-and-eve-porcello/clear-react/children/main.jsx
@@ -0,0 +1,12 @@
+let ul = React.createElement(
+    "ul",
+    null,
+    React.createElement("li", null, "1 lb Salmon"),
+    React.createElement("li", null, "1 cup Pine Nuts"),
+    React.createElement("li", null, "2 cups Butter Lettuce"),
+    React.createElement("li", null, "1 Yellow Squash"),
+    React.createElement("li", null, "1/2 cup Olive Oil"),
+    React.createElement("li", null, "3 cloves of Garlic"),
+);
+
+ReactDOM.render(ul, document.querySelector(".react-container"));
